deca5962bac0a35ab5f549e75ab334fbaebf2624
alessandro.lapini1 <alessandro.lapini@conjurer.es>
2023-05-23 12:44:47 +0200

    Make it look decent

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CollapseNavbar from "./components/navbar.jsx";
+import UncontrolledCarousel from "./components/carousel.jsx"
+import Footer from "./components/footer.jsx"; 
+
+function App() {
+  return (
+    <>
+    <CollapseNavbar/>
+    <UncontrolledCarousel/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
